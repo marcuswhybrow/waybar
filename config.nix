5b420723c14daf01bbaa24629c463852f3589ac2
Marcus Whybrow <marcus@whybrow.uk>
2023-11-30 16:27:03 +0000

    Initial commit

---
+++ config.nix
@@ -0,0 +1,141 @@
+{ pkgs, inputs }: let 
+  icon = text: ''<span color="#fff" font_family="Font Awesome 6 Free">${text}</span>'';
+  updates = "${inputs.nixpkgs-updates.packages.x86_64-linux.nixpkgs-updates}/bin/nixpkgs-updates";
+  networking = "${inputs.networking.packages.x86_64-linux.networking}/bin/networking";
+  alacritty = "${inputs.alacritty.packages.x86_64-linux.alacritty}/bin/alacritty";
+  htop = "${pkgs.htop}/bin/htop";
+  open = "${pkgs.xdg-utils}/bin/xdg-open";
+  qdirstat = "${pkgs.qdirstat}/bin/qdirstat";
+in builtins.toJSON {
+  layer = "top";
+  position = "top";
+  mode = "dock";
+  ipc = false;
+  margin = "10 20 -10 20";
+  height = null;
+  width = null;
+
+  modules-left = [
+    "custom/wifi-alarm"
+    "network"
+    "cpu"
+    "memory"
+    "temperature"
+    "disk"
+    "battery"
+  ];
+
+  modules-center = [
+    "hyprland/workspaces"
+    "tray"
+  ];
+
+  modules-right = [
+    # "bluetooth"
+    "custom/updates"
+    "clock#date"
+  ];
+
+  tray = {
+    icon-size = 21;
+    spacing = 10;
+  };
+
+  # https://github.com/Alexays/Waybar/wiki/Module:-Network
+  network = {
+    interval = 1;
+    on-click = ''${networking}'';
+
+    format = "${icon ""} Disabled";
+    tooltip = "Networking disabled";
+
+    format-wifi = "${icon ""} {signalStrength:02}%";
+    tooltip-format-wifi = "{essid} {signalStrength}% {ipaddr}";
+
+    format-ethernet = "${icon ""} {ipaddr}";
+    tooltip-format-ethernet = "Ethernet {ipaddr}";
+
+    format-disconnected = "${icon ""} Disconnected";
+    tooltip-format-disconnected = "Disconnected";
+  };
+
+  "custom/wifi-alarm" = {
+    # This detmines if the WiFi radio is on
+    # Ref: https://unix.stackexchange.com/questions/260235/command-to-detect-if-internet-connection-is-wired-or-wireless
+    exec = ''
+      ip route get 8.8.8.8 2> /dev/null | \
+      grep -Po 'dev \K\w+' | \
+       grep -qFf - /proc/net/wireless \
+      || [[ $(nmcli radio wifi) == enabled ]] \
+      && echo '⚠️'
+    '';
+    interval = 1;
+    on-click = "${networking}";
+  };
+
+  "custom/updates" = {
+    exec = "${updates} ~/.nixos";
+    on-click = "${alacritty} --working-directory ~/.nixos";
+    interval = 1;
+  };
+
+  cpu = {
+    format = "${icon ""} {usage:02}%";
+    interval = 1;
+    states = {
+      warning = 70;
+      critical = 90;
+    };
+    on-click = "${alacritty} --command ${htop} --sort-key=PERCENT_CPU";
+  };
+
+  memory = {
+    interval = 1;
+    format = ''${icon ""} {percentage:02}%'';
+    tooltip-format = "{used:0.1f}/{total:0.1f}GB RAM";
+    states = {
+      warning = 70;
+      critical = 90;
+    };
+    on-click = "${alacritty} --command ${htop} --sort-key=PERCENT_MEM";
+  };
+
+  temperature = {
+    interval = 1;
+    format = "${icon ""} {temperatureC:02}°C";
+    tooltip-format = "{temperatureC}°C";
+    critical-threshold = 88;
+  };
+
+  disk = {
+    interval = 60;
+    format = "${icon ""} {percentage_used:02}%";
+    tooltip-format = "{used} of {total} SSD";
+    on-click = "${qdirstat} ~";
+  };
+
+  # https://github.com/Alexays/Waybar/wiki/Module:-Battery
+  battery = {
+    interval = 1;
+    format = "${icon ""} {capacity:02}%";
+    tooltip-format = "{timeTo}";
+
+    format-charging = "${icon ""} {capacity:02}%";
+    tooltip-format-charging = "{timeTo}";
+
+    format-discharging = "${icon ""} {capacity:02}%";
+    tooltip-format-discharging = "{timeTo}";
+
+    states = {
+      good = 95;
+      warning = 20;
+      critical = 10;
+    };
+  };
+
+  "clock#date" = {
+    tooltip = false;
+    format = "{:%a %d %b %H:%M}"; # https://fmt.dev/dev/syntax.html#chrono-specs
+    on-click = "${open} https://calendar.proton.me/u/1";
+  };
+}
---
+++ flake.nix
@@ -0,0 +1,39 @@
+{
+  description = "Waybar (status bar) configure by Marcus";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    networking.url = "github:marcuswhybrow/networking";
+    nixpkgs-updates.url = "github:marcuswhybrow/nixpkgs-updates";
+    alacritty.url = "github:marcuswhybrow/alacritty";
+  };
+
+  outputs = inputs: let 
+    pkgs = inputs.nixpkgs.legacyPackages.x86_64-linux;
+
+    configText = import ./config.nix { inherit pkgs inputs; };
+    config = pkgs.writeText "config" configText;
+
+    styleText = builtins.readFile ./style.css;
+    style = pkgs.writeText "style.css" styleText;
+
+    wrapper = pkgs.runCommand "waybar-wrapper" {
+      nativeBuildInputs = [ pkgs.makeWrapper ];
+    } ''
+      mkdir --parents $out/share
+      ln -s ${config} $out/share/config
+      ln -s ${style} $out/share/style.css
+
+      mkdir --parents $out/bin
+      makeWrapper ${pkgs.waybar}/bin/waybar $out/bin/waybar \
+        --add-flags "--config $out/share/config --style $out/share/style.css"
+    '';
+  in {
+    packages.x86_64-linux.waybar = pkgs.symlinkJoin {
+      name = "waybar";
+      paths = [ wrapper pkgs.waybar ]; # first ./bin/waybar has precedence
+    };
+
+    packages.x86_64-linux.default = inputs.self.packages.x86_64-linux.waybar;
+  };
+}
