626d43cf1dc1695270af24220abcd056a4d205b2
Marcus Whybrow <marcus@whybrow.uk>
2024-05-04 16:50:21 +0100

    Fix repo location for NixOS updates plugin

--- config.nix
+++ config.nix
@@ -77,8 +77,8 @@ in builtins.toJSON {
   };
 
   "custom/updates" = {
-    exec = "${flake-updates} --flake ~/Repositories/nixos --output ' %s'";
-    on-click = "${alacritty} --working-directory ~/Repositories/nixos";
+    exec = "${flake-updates} --flake ~/Repos/nixos --output ' %s'";
+    on-click = "${alacritty} --working-directory ~/Repos/nixos";
     interval = 1;
   };
 
