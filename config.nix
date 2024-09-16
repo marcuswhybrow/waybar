a546cda99b63691cb1d13fc021e57b606c4c58a9
Marcus Whybrow <marcus@whybrow.uk>
2024-09-16 18:59:19 +0100

    Replace mwpkgs with individual inputs

--- config.nix
+++ config.nix
@@ -1,10 +1,10 @@
-{ pkgs, mwpkgs, ... }: let 
+{ pkgs, inputs, ... }: let 
   icon = text: ''<span color="#fff" font_family="Font Awesome 6 Free">${text}</span>'';
-  flake-updates = "${mwpkgs.flake-updates}/bin/flake-updates";
-  networking = "${mwpkgs.networking}/bin/networking";
-  alacritty = "${mwpkgs.alacritty}/bin/alacritty";
-  logout = "${mwpkgs.logout}/bin/logout";
-  rofi = "${mwpkgs.rofi}/bin/rofi";
+  flake-updates = "${inputs.flake-updates.packages.x86_64-linux.flake-updates}/bin/flake-updates";
+  networking = "${inputs.networking.packages.x86_64-linux.networking}/bin/networking";
+  alacritty = "${inputs.alacritty.packages.x86_64-linux.alacritty}/bin/alacritty";
+  logout = "${inputs.logout.packages.x86_64-linux.logout}/bin/logout";
+  rofi = "${inputs.rofi.packages.x86_64-linux.rofi}/bin/rofi";
   htop = "${pkgs.htop}/bin/htop";
   open = "${pkgs.xdg-utils}/bin/xdg-open";
   qdirstat = "${pkgs.qdirstat}/bin/qdirstat";
--- flake.nix
+++ flake.nix
@@ -3,38 +3,34 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    mwpkgs = {
-      url = "github:marcuswhybrow/mwpkgs";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    flake-updates.url = "github:marcuswhybrow/flake-updates";
+    networking.url = "github:marcuswhybrow/networking";
+    alacritty.url = "github:marcuswhybrow/alacritty";
+    logout.url = "github:marcuswhybrow/logout";
+    rofi.url = "github:marcuswhybrow/rofi";
   };
 
   outputs = inputs: let 
     pkgs = inputs.nixpkgs.legacyPackages.x86_64-linux;
-    mwpkgs = inputs.mwpkgs.packages.x86_64-linux;
-    waybar = pkgs.waybar;
-
-    configText = import ./config.nix { inherit pkgs mwpkgs; };
-    config = pkgs.writeText "config" configText;
-
-    styleText = builtins.readFile ./style.css;
-    style = pkgs.writeText "style.css" styleText;
-
-    wrapper = pkgs.runCommand "waybar-wrapper" {
-      nativeBuildInputs = [ pkgs.makeWrapper ];
-    } ''
-      mkdir --parents $out/share
-      ln -s ${config} $out/share/config
-      ln -s ${style} $out/share/style.css
-
-      mkdir --parents $out/bin
-      makeWrapper ${waybar}/bin/waybar $out/bin/waybar \
-        --add-flags "-c $out/share/config --style $out/share/style.css"
-    '';
   in {
     packages.x86_64-linux.waybar = pkgs.symlinkJoin {
       name = "waybar";
-      paths = [ wrapper waybar ]; # first ./bin/waybar has precedence
+      paths = [ 
+
+        (pkgs.runCommand "waybar-wrapper" {
+          nativeBuildInputs = [ pkgs.makeWrapper ];
+        } ''
+          mkdir --parents $out/share
+          ln -s ${pkgs.writeText "config" (import ./config.nix { inherit pkgs inputs; })} $out/share/config
+          ln -s ${pkgs.writeText "style.css" (builtins.readFile ./style.css)} $out/share/style.css
+
+          mkdir --parents $out/bin
+          makeWrapper ${pkgs.waybar}/bin/waybar $out/bin/waybar \
+            --add-flags "-c $out/share/config --style $out/share/style.css"
+        '')
+
+        pkgs.waybar 
+      ];
     };
 
     packages.x86_64-linux.default = inputs.self.packages.x86_64-linux.waybar;
